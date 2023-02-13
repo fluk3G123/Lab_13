146b94ac16a909ca2be32139f88edf6c9df46a73
__.flxk.__ <89276072+fluk3G123@users.noreply.github.com>
2023-02-13 20:43:13 +0700

    bye

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template<class T >
+void mySwap(T &x,T &y);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+template<class T >
+void mySwap(T &x,T &y ){
+	T kap = x;
+	x = y;
+	y= kap ;
+
 }
 
